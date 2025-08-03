a2e4ebfa5c3db6fabed2b8b410f348e808836b01
ozgungungor <ozgun.gungor>
2025-08-03 12:37:23 +0300

    Initial Commit

---
+++ faizhesapla/faizhesaplaApp.swift
@@ -0,0 +1,32 @@
+//
+//  faizhesaplaApp.swift
+//  faizhesapla
+//
+//  Created by özgün güngör on 3.08.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct faizhesaplaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ faizhesaplaTests/faizhesaplaTests.swift
@@ -0,0 +1,17 @@
+//
+//  faizhesaplaTests.swift
+//  faizhesaplaTests
+//
+//  Created by özgün güngör on 3.08.2025.
+//
+
+import Testing
+@testable import faizhesapla
+
+struct faizhesaplaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
